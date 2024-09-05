5127361a89c2818da144445b3244dce746ba8b39
bugxch <bugxch@gmail.com>
2024-09-05 22:47:59 +0800

    add lib test

--- test/main.cpp
+++ test/main.cpp
@@ -1,15 +1,23 @@
 #include "gtest/gtest.h"
 #include <iostream>
 #include <string>
+#include "item1.h"
 
-TEST(Item1, Test1) {
+TEST(Item1, Test1)
+{
 	EXPECT_EQ(1, 1);
 }
 
-TEST(Item1, Test2) {
+TEST(Item1, Test2)
+{
 	EXPECT_EQ(3, 1 + 2);
 }
 
+TEST(Item1, Test3)
+{
+	Item1 item;
+	EXPECT_LE(6.0f, item.GetArea());
+}
 
 int main(int argc, char **argv)
 {
